062aca32f4cbce35e7103f2966130da1e8a7e9a2
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-02 12:05:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -31,6 +31,7 @@ while director_index < nds.length do
   result[nds[director_index][:name]] = gross_for_director(directors_database[director_index])
   director_index += 1
 end 
+result
 #result[:name] = gross_for_director(nds)
 #director_index = 0
 #while director_index < 3 do
