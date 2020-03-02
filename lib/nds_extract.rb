196b86b86440b804d08a1770397978723c3641a7
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-02 11:44:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,7 +24,7 @@ puts nds
 puts nds
 puts nds
 result = {}
-puts gross_for_director(directors_data)
+puts gross_for_director(director_data)
 #result[:name] = gross_for_director(nds)
 #director_index = 0
 #while director_index < 3 do
