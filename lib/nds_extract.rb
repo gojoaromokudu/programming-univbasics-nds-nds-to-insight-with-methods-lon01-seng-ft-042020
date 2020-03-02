5c677035a169ec06ee41f426d3a9695f4288731f
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-02 12:06:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,40 +20,11 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
-puts nds
-puts nds
-
 result = {}
-puts gross_for_director(directors_database[2])
-puts nds[0][:name]
 director_index = 0
 while director_index < nds.length do
   result[nds[director_index][:name]] = gross_for_director(directors_database[director_index])
   director_index += 1
 end 
 result
-#result[:name] = gross_for_director(nds)
-#director_index = 0
-#while director_index < 3 do
-#puts gross_for_director(nds[0])
-#director_index += 1 
-#end
-
-#  pp directors_database
-#puts directors_database[0]
-#puts director_data
- 
-  
- # director_index = 0 
- # while director_index < nds.length do
- #   movie_index = 0
-  #  grosses = 0 
-  #  while movie_index < nds[director_index][:movies].length do
-  #    grosses += nds[director_index][:movies][movie_index][:worldwide_gross]
- #     puts nds[director_index][:movies][movie_index][:worldwide_gross]
-  #    movie_index += 1 
- #   end
-  #  result[[nds[director_index][:name]]] = grosses
- # end
- # result
 end
